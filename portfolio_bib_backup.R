f851d82c3f06eb7e5da0397bd3182a0f3c4885b9
stephaniedewit <stephanie.dewit@student.hu.nl>
2022-06-03 22:45:32 +0200

    Rommelen met Zotero, bib backup

---
+++ portfolio_bib_backup.R
@@ -0,0 +1,114 @@
+@article{broadleyLangendorffHeartPreparation1979,
+  title = {The {{Langendorff}} Heart Preparation—{{Reappraisal}} of Its Role as a Research and Teaching Model for Coronary Vasoactive Drugs},
+  author = {Broadley, K. J.},
+  date = {1979-05-01},
+  journaltitle = {Journal of Pharmacological Methods},
+  shortjournal = {Journal of Pharmacological Methods},
+  volume = {2},
+  number = {2},
+  pages = {143--156},
+  issn = {0160-5402},
+  doi = {10.1016/0160-5402(79)90038-X},
+  url = {https://www.sciencedirect.com/science/article/pii/016054027990038X},
+  urldate = {2022-05-14},
+  abstract = {The historical development and improvement of the Langendorff isolated perfused heart is traced with particular emphasis on its use for recording coronary vascular responses to drugs. A range of coronary vasoactive drugs are examined on a modified sensitive preparation. These drugs—catecholamines, 5-HT, histamine, acetylcholine, bradykinin, vasopressin, angiotensin, papaverine, sodium nitrite, and aminophylline—exhibited classical responses that compare favorably with those reported in the literature and with the in vivo responses. The actions of these drugs are reviewed and the advantages and limitations of the preparation as a teaching and research model are discussed.},
+  langid = {english},
+  keywords = {Coronary vascular responses,Inotropic and chronotropic responses,Perfused heart},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\R9A5KLE8\\016054027990038X.html}
+}
+
+@article{kazandjianConvergentEvolutionPainInducing2021,
+  title = {Convergent {{Evolution}} of {{Pain-Inducing Defensive Venom Components}} in {{Spitting Cobras}}},
+  author = {Kazandjian, T.D. and Petras, D. and Robinson, S.D. and van Thiel, J. and Greene, H.W. and Arbuckle, K. and Barlow, A. and Carter, D.A. and Wouters, R.M. and Whiteley, G. and Wagstaff, S.C. and Arias, A.S. and Albulescu, L-O. and Plettenberg Laing, A. and Hall, C. and Heap, A. and Penrhyn-Lowe, S. and McCabe, C.V. and Ainsworth, S. and da Silva, R.R. and Dorrestein, P.C. and Richardson, M.K. and Gutiérrez, J.M. and Calvete, J.J. and Harrison, R.A. and Vetter, I. and Undheim, E.A.B. and Wüster, W. and Casewell, N.R.},
+  options = {useprefix=true},
+  date = {2021-01-22},
+  journaltitle = {Science (New York, N.Y.)},
+  shortjournal = {Science},
+  volume = {371},
+  number = {6527},
+  eprint = {33479150},
+  eprinttype = {pmid},
+  pages = {386--390},
+  issn = {0036-8075},
+  doi = {10.1126/science.abb9303},
+  url = {https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7610493/},
+  urldate = {2022-05-14},
+  abstract = {Convergent evolution provides insights into the selective drivers underlying evolutionary change. Snake venoms, with a direct genetic basis and clearly defined functional phenotype, provide a model system for exploring the repeated evolution of adaptations. While snakes use venom primarily for predation, and venom composition often reflects diet specificity, three lineages of cobras have independently evolved the ability to spit venom at adversaries. Using gene, protein and functional analyses, we show that the three spitting lineages possess venoms characterized by an upregulation of PLA2 toxins, which potentiate the action of pre-existing venom cytotoxins to activate mammalian sensory neurons and cause enhanced pain. These repeated independent changes provide a fascinating example of convergent evolution across multiple phenotypic levels driven by selection for defense.},
+  pmcid = {PMC7610493},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\S7MFTMKM\\Kazandjian e.a. - 2021 - Convergent Evolution of Pain-Inducing Defensive Ve.pdf}
+}
+
+@online{ProjectRegenwormenData,
+  title = {Project {{Regenwormen}}: {{Data Sciences}} for {{Biology}} 2},
+  url = {https://canvas.hu.nl/courses/26999/pages/project-regenwormen?module_item_id=720432},
+  urldate = {2022-05-07},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\2KTYYCIN\\project-regenwormen.html}
+}
+
+@online{ThemaRegenwormDOFAR,
+  title = {Thema 3: {{Regenworm}}: {{DOFAR Dierkundig Onderzoek}}},
+  url = {https://canvas.hu.nl/courses/23205/pages/thema-3-regenworm},
+  urldate = {2022-05-14},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\6SGT7QVK\\thema-3-regenworm.html}
+}@online{LLThreeparameterLoglogistic,
+  title = {{{LL}}.3: {{The}} Three-Parameter Log-Logistic Function in Drc: {{Analysis}} of {{Dose-Response Curves}}},
+  shorttitle = {{{LL}}.3},
+  url = {https://rdrr.io/cran/drc/man/LL.3.html},
+  urldate = {2022-05-24},
+  abstract = {'LL.3' and 'LL2.3' provide the three-parameter log-logistic function where the lower limit is equal to 0.  'LL.3u' and 'LL2.3u' provide three-parameter logistic function where the upper limit is equal to 1, mainly for use with binomial/quantal response.},
+  langid = {english}
+}
+
+@article{ritzDoseResponseAnalysisUsing2015,
+  title = {Dose-{{Response Analysis Using R}}},
+  author = {Ritz, Christian and Baty, Florent and Streibig, Jens C. and Gerhard, Daniel},
+  date = {2015-12-30},
+  journaltitle = {PLOS ONE},
+  shortjournal = {PLOS ONE},
+  volume = {10},
+  number = {12},
+  pages = {e0146021},
+  publisher = {{Public Library of Science}},
+  issn = {1932-6203},
+  doi = {10.1371/journal.pone.0146021},
+  url = {https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0146021},
+  urldate = {2022-05-24},
+  abstract = {Dose-response analysis can be carried out using multi-purpose commercial statistical software, but except for a few special cases the analysis easily becomes cumbersome as relevant, non-standard output requires manual programming. The extension package drc for the statistical environment R provides a flexible and versatile infrastructure for dose-response analyses in general. The present version of the package, reflecting extensions and modifications over the last decade, provides a user-friendly interface to specify the model assumptions about the dose-response relationship and comes with a number of extractors for summarizing fitted models and carrying out inference on derived parameters. The aim of the present paper is to provide an overview of state-of-the-art dose-response analysis, both in terms of general concepts that have evolved and matured over the years and by means of concrete examples.},
+  langid = {english},
+  keywords = {Binomials,Computer software,Curve fitting,Dose prediction methods,Log dose-response method,Nonlinear least squares method,Normal distribution,User interfaces},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\ALEIR2L4\\Ritz e.a. - 2015 - Dose-Response Analysis Using R.pdf;C\:\\Users\\steph\\Zotero\\storage\\3MEWLIYT\\article.html}
+}
+
+@online{RPubsFourparameterLoglogistic,
+  title = {{{RPubs}} - Four-Parameter Log-Logistic Model},
+  url = {https://rpubs.com/TX-YXL/656451},
+  urldate = {2022-05-24}
+}
+
+@online{ThreeparameterLoglogisticFunction,
+  title = {The Three-Parameter Log-Logistic Function — {{LL}}.3},
+  url = {https://doseresponse.github.io/drc/reference/LL.3.html},
+  urldate = {2022-05-24},
+  abstract = {'LL.3' and 'LL2.3' provide the three-parameter log-logistic function where the lower limit is equal to 0.   'LL.3u' and 'LL2.3u' provide three-parameter logistic function where the upper limit is equal to 1, mainly   for use with binomial/quantal response.},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\PDWN4STP\\LL.3.html}
+}
+
+@article{chaimayoRapidSARSCoV2Antigen2020,
+  title = {Rapid {{SARS-CoV-2}} Antigen Detection Assay in Comparison with Real-Time {{RT-PCR}} Assay for Laboratory Diagnosis of {{COVID-19}} in {{Thailand}}},
+  author = {Chaimayo, Chutikarn and Kaewnaphan, Bualan and Tanlieng, Nattaya and Athipanyasilp, Niracha and Sirijatuphat, Rujipas and Chayakulkeeree, Methee and Angkasekwinai, Nasikarn and Sutthent, Ruengpung and Puangpunngam, Nattawut and Tharmviboonsri, Theerawoot and Pongraweewan, Orawan and Chuthapisith, Suebwong and Sirivatanauksorn, Yongyut and Kantakamalakul, Wannee and Horthongkham, Navin},
+  date = {2020-11-13},
+  journaltitle = {Virology Journal},
+  shortjournal = {Virology Journal},
+  volume = {17},
+  number = {1},
+  pages = {177},
+  issn = {1743-422X},
+  doi = {10.1186/s12985-020-01452-5},
+  url = {https://doi.org/10.1186/s12985-020-01452-5},
+  urldate = {2022-05-25},
+  abstract = {The Coronavirus disease 2019 (COVID-19) pandemic continues to spread across the world. Hence, there is an urgent need for rapid, simple, and accurate tests to diagnose severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2) infection. Performance characteristics of the rapid SARS-CoV-2 antigen detection test should be evaluated and compared with the gold standard real-time reverse transcription-polymerase chain reaction (RT-PCR) test for diagnosis of COVID-19 cases.},
+  keywords = {COVID-19,Rapid antigen,RT-PCR,SARS-CoV-2,Thailand},
+  file = {C\:\\Users\\steph\\Zotero\\storage\\M2W5TGIP\\Chaimayo e.a. - 2020 - Rapid SARS-CoV-2 antigen detection assay in compar.pdf;C\:\\Users\\steph\\Zotero\\storage\\HY8ZTTTM\\s12985-020-01452-5.html}
+}
+
+
